b93a285090fe86cd26ab077b5c51135615647f8d
BrijendraK <BrijendraK@LP-5CD8471476.HCLT.CORP.HCL.IN>
2019-08-08 15:35:33 +0530

    Code refactoring. logging changes.

--- src/main/java/com/ulta/product/controller/ProductController.java
+++ src/main/java/com/ulta/product/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.ulta.product.controller;
 
+import static com.ulta.product.constant.ProductConstants.VIEW_PRODUCT_BYPRODUCTKEY_URI;
+
 import java.io.IOException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +13,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.ulta.product.constant.ProductConstants.VIEW_PRODUCT_BYPRODUCTKEY_URI;
 import com.ulta.product.exception.ProductException;
 import com.ulta.product.service.ProductService;
 import com.ulta.product.serviceImpl.ClientService;
+
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.products.Product;
 
@@ -61,20 +63,24 @@ public class ProductController {
 
 			if (null != products.get()) {
 				product = products.get();
-				System.out.println("config Server value::::" +message);
+				log.info("get the product details successfully.");
 			} else {
+				log.info("getting product details as null");
 				throw new ProductException(message);
 			}
 
 		} catch (ProductException ex) {
+			log.error("exception during fetching the product detail-" + ex.getMessage());
 			throw new ProductException(ex.getMessage());
 		} catch (InterruptedException ex) {
+			log.error("exception during fetching the product detail-" + ex.getMessage());
 			throw new ProductException(ex.getMessage());
 
 		} catch (ExecutionException ex) {
+			log.error("exception during fetching the product detail-" + ex.getMessage());
 			throw new ProductException(ex.getMessage());
 		}
-		log.info("getProductByKey method start");
+		log.info("getProductByKey method end");
 		return ResponseEntity.ok().body(product);
 	}
 }
--- src/main/java/com/ulta/product/serviceImpl/ProductServiceImpl.java
+++ src/main/java/com/ulta/product/serviceImpl/ProductServiceImpl.java
@@ -3,6 +3,8 @@ package com.ulta.product.serviceImpl;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.ulta.product.service.ProductService;
@@ -17,13 +19,14 @@ import io.sphere.sdk.products.queries.ProductByKeyGet;
  */
 @Service
 public class ProductServiceImpl implements ProductService {
-
+	static Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
 	@Override
 	public CompletableFuture<Product> getProductByKey(SphereClient client, String key) {
-		
+		 log.info("getProductByKey method start");
 		 final ProductByKeyGet request = ProductByKeyGet.of(key);
 		 CompletionStage<Product> pro= client.execute(request);
 		 CompletableFuture<Product> returnProduct =pro.toCompletableFuture();
+		 log.info("getProductByKey method end");
 		 return returnProduct;
 	}
 
